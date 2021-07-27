3b6424af712ae8abb181c54fea7a27fa7eca4c74
Prathima Kadari <kadariprathima4@gmail.com>
2021-07-27 23:27:06 +0530

    Add files via upload

---
+++ project/eportal/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from eportal import views
+
+app_name = 'eportal'
+
+urlpatterns = [
+    path('',views.index,name="home"),
+    path('ocr/',views.ocr_page,name="ocr_page"),
+    path('ocr_output/',views.ocr_output,name="ocr_output"),
+    path('synthesizer/',views.synthesizer,name="synthesizer"),
+    path('our_vision/',views.our_vision,name="our_vision"),
+]
---
+++ project/eportal/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .ocr import main_url
+# Create your views here.
+
+def ocr_page(request):
+    return render(request, 'eportal/ocr_form.html', {})
+
+def ocr_output(request):
+    if request.method == "POST":
+        if 'ocr_file' in request.FILES:
+            file = request.FILES['ocr_file']
+            final_output = main_url(file)
+            text = ''
+            for output in final_output:
+                text = text + output
+            return render(request,'eportal/ocr_output.html',{'output':text})
+
+def index(request):
+    return render(request,'eportal/index.html',{})
+
+def synthesizer(request):
+    return render(request,'eportal/synthsizer.html',{})
+
+def our_vision(request):
+    return render(request,'eportal/our_vision.html',{})    
